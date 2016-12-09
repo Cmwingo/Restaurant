461506e7025e609c7a46e2a1f08c4a551d12a65e
Wingo <cmwingo@gmail.com>
2016-12-08 16:28:41 -0800

    Work with web interface

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -29,9 +29,36 @@ namespace RestaurantReview
         return View["success.cshtml"];
       };
 
+      Get["/cuisines/{id}"] = parameters => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var SelectedCuisine = Cuisine.Find(parameters.id);
+        var CuisineRestaurants = SelectedCuisine.GetRestaurants();
+        model.Add("cuisine", SelectedCuisine);
+        model.Add("restaurants", CuisineRestaurants);
+        return View["cuisine.cshtml", model];
+      };
+
       Get["/restaurants"] = _ => {
         List<Restaurant> AllRestaurants = Restaurant.GetAll();
         return View["restaurants.cshtml", AllRestaurants];
+      };
+
+      Get["/restaurants/new"] = _ => {
+        List<Cuisine> AllCuisines = Cuisine.GetAll();
+        return View["restaurants_form.cshtml", AllCuisines];
+      };
+
+      Post["/restaurants/new"] = _ => {
+        bool delivery = Request.Form["delivery"];
+        Console.WriteLine(delivery);
+        Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-name"], Request.Form["restaurant-location"], Request.Form["restaurant-description"], Request.Form["avg-cost"], delivery, Request.Form["cuisine-id"]);
+        newRestaurant.Save();
+        return View["success.cshtml"];
+      };
+
+      Post["/restaurants/delete"] = _ => {
+        Restaurant.DeleteAll();
+        return View["cleared.cshtml"];
       };
     }
   }
